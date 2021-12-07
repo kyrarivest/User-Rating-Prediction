ce34c10f69c556d35896b1c70e5f4eb057591def
Kyra Rivest <57569515+kyrarivest@users.noreply.github.com>
2021-12-06 21:51:05 -0500

    Update build_WEIGHT_matrix.py

--- build_WEIGHT_matrix.py
+++ build_WEIGHT_matrix.py
@@ -2,7 +2,16 @@ import numpy as np
 import pandas as pd
 import pickle
 
+#Author: Daniel Hariyanto
+#Description: builds a matrix of the 
 
+
+
+
+#Returns the inverse of the weighted distance between the absolute distance of two user's browser times
+#To be used in the build function to create the weight matrix
+#abs_diff: the absolute distance of two user's website browsing times for a particualr website
+#exp: the specified power to raise the abs_diff by. This is a parameter of the model that is varied
 def get_weights_distance(abs_diff, exp):
     tot_abs_diff = np.sum(abs_diff)
     if tot_abs_diff == 0:
@@ -11,21 +20,23 @@ def get_weights_distance(abs_diff, exp):
 
 
 
+#The main function that builds the weight matrix
+#user_history: the given user_history csv file
 def build(user_history):
-    ### Finding weight matrix
-
+    print("Begin building weight matrix...")
     weight_matrix = []
 
-    print('finding weight matrix')
-
-    for u in range(len(user_history)):
+    
+    for u in range(len(user_history)):  #for each user row
         weight_matrix.append([float(0) for i in range(u)])
-        for v in range(u, len(user_history)):
-            print('comparing User #' + str(u) + ' and ' + 'User #' + str(v))
+        
+        for v in range(u, len(user_history)):   #go through every user
+            print('     comparing User #' + str(u) + ' and ' + 'User #' + str(v))
             abs_diff = np.absolute(user_history.iloc[u, 1:] - user_history.iloc[v, 1:])
             tot_abs_diff = get_weights_distance(abs_diff, 2)
             weight_matrix[u].append(tot_abs_diff)
 
+    #Create matrix
     X = np.array(weight_matrix)
     X = X + X.T - np.diag(np.diag(X))
 
@@ -33,7 +44,11 @@ def build(user_history):
     for i in range(len(X)):
         weight_matrix[int(user_history.iloc[i][0])] = X[i]
 
+    #Create dataframe for weight matrix
     weight_matrix = pd.DataFrame(weight_matrix)
-    weight_matrix.to_csv('weight_matrix.csv')
+    
+    #Save weight matrix as csv file
+    #weight_matrix.to_csv('weight_matrix.csv')
 
     print('completed finding weight matrix')
+    return weight_matrix
