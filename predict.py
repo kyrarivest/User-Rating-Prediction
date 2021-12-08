db72911b6c57a6d622d285a62529620b99d86240
Kyra Rivest <57569515+kyrarivest@users.noreply.github.com>
2021-12-08 00:59:19 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import pickle
+import datetime
 
 #Authors: Daniel Hariyanto and Kyra Rivest
 #See the README.md for description and purpose of this file
@@ -51,7 +52,8 @@ def pred_ratings(weights_index, weights_vector, ratings_index, ratings_vector):
 #user_ratings_table: the ratings matrix of user and product ratings
 #power: specifies the power parameter
 def run(user_history, weight_matrix, user_ratings_table, power):
-    print('Predicting ratings...')
+    begin_time = datetime.datetime.now()
+    print('Begin predicting ratings...')
 
 
     columns = [col for col in user_ratings_table.columns]   #list of all product columns
@@ -59,11 +61,14 @@ def run(user_history, weight_matrix, user_ratings_table, power):
     user_ratings_table_pred = user_ratings_table.copy()     #copy of the user_ratings table to be filled in with ratings
 
     for u in range(len(user_ratings_table)):       #for each user
+        print("Processing user: " + str(u))
         for p in range(len(user_ratings_table.iloc[u])):    #predict rating for each product
-            pred = pred_ratings(list(user_history['USER ID']), weight_matrix[list(user_ratings_table.index)[u]], list(user_ratings_table[columns[p]].index), user_ratings_table[columns[p]])
+            pred = pred_ratings(list(user_history['USER ID']), weight_matrix[u], list(user_ratings_table[columns[p]].index), user_ratings_table[columns[p]])
             user_ratings_table_pred.iloc[u][p] = pred
 
+    print()
     print('Completed predicting ratings')
+    print("Execution time = " + str(datetime.datetime.now() - begin_time))
 
     #Save rating results table as a csv file
     if(power == 0.5):
