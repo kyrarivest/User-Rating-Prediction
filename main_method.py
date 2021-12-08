c6849231c19403a4008e40ed7c25d1947dfa844c
Kyra Rivest <57569515+kyrarivest@users.noreply.github.com>
2021-12-08 17:34:43 -0500

    Update main_method.py

--- main_method.py
+++ main_method.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-import pickle
+import pickle as pkl
 
 import build_WEIGHT_matrix
-import build_RATINGS_dfs
+import build_RATINGS_matrix
 import predict
 import analyze_results
 
@@ -11,15 +11,17 @@ import analyze_results
 
 user_history = pd.read_csv("user_history.csv")
 user_ratings = pd.read_csv("user_ratings.csv")
+RATINGS_matrix = build_RATINGS_dfs.build(user_history, user_ratings)
 
 
 #__________________MAIN METHOD__________________#
 
 powers = [0.5,2,4,8,16,32]
-RATINGS_matrix = build_RATINGS_dfs.build(user_history, user_ratings)
 
 for power in powers:
+    print("Computing for power: " + str(power))
+    WEIGHT_matrix = build_WEIGHT_matrix.build(user_history, power)
+    predicted_ratings = predict.run(user_history, WEIGHT_matrix, RATINGS_matrix, power)
 
-  WEIGHT_matrix = build_WEIGHT_matrix.build(user_history, power)
-  predicted_ratings = predict.run(WEIGHT_matrix, RATINGS_matrix, power)
-  analyze_results.run()
+
+analyze_results.run(RATINGS_matrix)
