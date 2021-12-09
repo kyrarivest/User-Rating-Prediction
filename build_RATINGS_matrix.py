8098ba5b0e6563585f30e31442f7a7fa793ba87e
Kyra Rivest <57569515+kyrarivest@users.noreply.github.com>
2021-12-08 21:12:25 -0500

    Update build_RATINGS_matrix.py

--- build_RATINGS_matrix.py
+++ build_RATINGS_matrix.py
@@ -8,23 +8,31 @@ import datetime
 
 
 
+#Main function that builds the user ratings matrix
+#user_history_data: the original user_history.csv file
+#user_ratings_data: the original user_ratings.csv file
+#Returns the user ratings matrix with all 4500 users, each as one row
+#Note: for those users that are in the user_history file but not the user_ratings file, they are added as a user with a completely blank row.
 def build(user_history_data, user_ratings_data):
     begin_time = datetime.datetime.now()
     print("Begin building ratings matrix...")
+    
+    
     # load data as a dataframe with pandas
     user_history = user_history_data
     user_ratings = user_ratings_data
 
+    
+    #Create the pivot table matrix for the 3000 users in user_ratings
     user_ratings_table = pd.pivot_table(user_ratings,values='RATING', index='USER ID', columns='PRODUCT')
 
-    #print(user_ratings_table)
-
+    
+    #Create a seperate dataframe for the extra 1500 users not in user_ratings
     extra_users = np.empty((1500,75,))
     extra_users[:] = np.nan
-    #print(list(user_ratings_table.columns))
-
     indices =[]
     user_ratings_indices = user_ratings_table.index
+    
     for user, data in user_history.iterrows():
        if user_history["USER ID"][user] not in user_ratings_indices:
            indices.append(user_history["USER ID"][user])
@@ -32,6 +40,8 @@ def build(user_history_data, user_ratings_data):
 
     extra_df = pd.DataFrame(extra_users, columns=user_ratings_table.columns, index=indices)
 
+    
+    #Combine the two dataframes 
     RATING_full_df = user_ratings_table.append(extra_df)
     RATING_full_df.index.name = "USER ID"
 
@@ -40,14 +50,7 @@ def build(user_history_data, user_ratings_data):
     print("Execution time = " + str(datetime.datetime.now() - begin_time))
     print()
     
-
-    print(RATING_full_df)
-    return RATING_full_df
-
     
-
-    
-
     #save ratings matrix as a csv and pkl file if needed
     """
     RATING_full_df.to_csv("RATINGS_full.csv", index=False)
@@ -56,6 +59,14 @@ def build(user_history_data, user_ratings_data):
     pickle.dump(RATING_full_df, open_file)
     open_file.close()
     """
+
+    return RATING_full_df
+
+    
+
+    
+
+    
 
 
     
